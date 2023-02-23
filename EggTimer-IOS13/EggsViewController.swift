16e8ce29fbdd0944edd88322dfd6940d11248106
emirmalika <emirmalika0712@gmail.com>
2023-02-23 20:47:55 +0600

    addSubview a title

--- EggTimer-IOS13/EggsViewController.swift
+++ EggTimer-IOS13/EggsViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class EggsViewController: UIViewController {
     
+    let label = UILabel()
     let eggsView = EggsView()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +24,22 @@ class EggsViewController: UIViewController {
 extension EggsViewController {
     private func style() {
         view.backgroundColor = UIColor((#colorLiteral(red: 0.7960784314, green: 0.9490196078, blue: 0.9882352941, alpha: 1)))
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "How do you like your eggs?"
+        label.textColor = .darkGray
+        label.font = UIFont.systemFont(ofSize: 30)
     }
     
     private func layout() {
+        view.addSubview(label)
         view.addSubview(eggsView)
         
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+       
         NSLayoutConstraint.activate([
             eggsView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             eggsView.centerYAnchor.constraint(equalTo: view.centerYAnchor),

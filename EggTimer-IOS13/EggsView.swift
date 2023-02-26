8d03b7629d9543030bcf98fd7f2ab7ff8942d95b
emirmalika <emirmalika0712@gmail.com>
2023-02-26 18:07:52 +0600

    add timer and calculate progressView

--- EggTimer-IOS13/EggsView.swift
+++ EggTimer-IOS13/EggsView.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class EggsView: UIView {
     
+    let label = UILabel()
     let softEggBtn = UIButton()
     let mediumEggBtn = UIButton()
     let hardEggBtn = UIButton()
-    
     let stackView = UIStackView()
+    let progressView = UIProgressView()
+    
+    let eggsTimeInSeconds = ["Soft": 3, "Medium": 4, "Hard": 7]
+    
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    var timer = Timer()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -36,12 +44,32 @@ extension EggsView {
         }
         
         softEggBtn.setBackgroundImage(UIImage(named: "soft_egg"), for: .normal)
+        softEggBtn.tag = 1
+        
         mediumEggBtn.setBackgroundImage(UIImage(named: "medium_egg"), for: .normal)
+        mediumEggBtn.tag = 2
+        
         hardEggBtn.setBackgroundImage(UIImage(named: "hard_egg"), for: .normal)
+        hardEggBtn.tag = 3
+        
+        [softEggBtn, mediumEggBtn, hardEggBtn].forEach {
+            $0.addTarget(self, action: #selector(eggBtnTapped), for: .touchUpInside)
+        }
         
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
         stackView.spacing = 16
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "How do you like your eggs?"
+        label.textColor = .darkGray
+        label.font = UIFont.systemFont(ofSize: 30)
+        
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        progressView.progressViewStyle = .bar
+        progressView.progressTintColor = .systemYellow
+        progressView.trackTintColor = .systemGray
+        progressView.progress = 0.0
     }
     
     private func layout() {
@@ -49,12 +77,58 @@ extension EggsView {
         stackView.addArrangedSubview(mediumEggBtn)
         stackView.addArrangedSubview(hardEggBtn)
         addSubview(stackView)
+        addSubview(label)
+        addSubview(progressView)
         
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: topAnchor),
-            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            label.topAnchor.constraint(equalTo: topAnchor, constant: 150),
+            label.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            stackView.heightAnchor.constraint(equalToConstant: 150)
             ])
+        
+        NSLayoutConstraint.activate([
+            progressView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            progressView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            progressView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -150)
+        ])
+    }
+}
+
+extension EggsView {
+    @objc func eggBtnTapped(_ sender: UIButton) {
+        timer.invalidate()
+        progressView.progress = 0.0
+        secondsPassed = 0
+        switch sender.tag {
+        case 1:
+            totalTime = eggsTimeInSeconds["Soft"]!
+            label.text = "Soft"
+        case 2:
+            totalTime = eggsTimeInSeconds["Medium"]!
+            label.text = "Medium"
+        default:
+            totalTime = eggsTimeInSeconds["Hard"]!
+            label.text = "Hard"
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            print("(Float(secondsPassed)/Float(totalTime)")
+            progressView.progress = Float(secondsPassed)/Float(totalTime)
+        } else {
+            timer.invalidate()
+            label.text = "DONE!"
+        }
     }
 }
--- EggTimer-IOS13/EggsViewController.swift
+++ EggTimer-IOS13/EggsViewController.swift
@@ -9,10 +9,8 @@ import UIKit
 
 class EggsViewController: UIViewController {
     
-    let label = UILabel()
     let eggsView = EggsView()
-    let progressView = UIProgressView()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,40 +22,16 @@ class EggsViewController: UIViewController {
 extension EggsViewController {
     private func style() {
         view.backgroundColor = UIColor((#colorLiteral(red: 0.7960784314, green: 0.9490196078, blue: 0.9882352941, alpha: 1)))
-        
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "How do you like your eggs?"
-        label.textColor = .darkGray
-        label.font = UIFont.systemFont(ofSize: 30)
-        
-        progressView.translatesAutoresizingMaskIntoConstraints = false
-        progressView.progressViewStyle = .bar
-        progressView.progressTintColor = .systemYellow
-        progressView.trackTintColor = .systemGray
     }
     
     private func layout() {
-        view.addSubview(label)
         view.addSubview(eggsView)
-        view.addSubview(progressView)
         
         NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        ])
-       
-        NSLayoutConstraint.activate([
-            eggsView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            eggsView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            eggsView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            eggsView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            eggsView.heightAnchor.constraint(equalToConstant: 150)
-        ])
-        
-        NSLayoutConstraint.activate([
-            progressView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            progressView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            progressView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -150)
+            eggsView.topAnchor.constraint(equalTo: view.topAnchor),
+            eggsView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            eggsView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            eggsView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
 }
